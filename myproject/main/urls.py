17a5c3312e8dcb903946f9812f9829ae2ddda061
MurkaATAT <m.alimbaev@baqsy.kz>
2025-11-12 11:57:09 +0500

    v2.1

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('products/', views.products_list, name='products'),
     path('orders/', views.orders_list, name='orders'),
     path('stocks/', views.stocks_list, name='stocks'),
+
+    path('products/', views.products_list, name='products'),
+    path('products/add/', views.product_create, name='product_add'),
+    path('products/<int:pk>/edit/', views.product_update, name='product_edit'),
+    path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm
+
+
 # Create your views here.
 from django.shortcuts import render
 from .models import Client, Product, Order, Stock
@@ -25,3 +30,37 @@ def orders_list(request):
 def stocks_list(request):
     stocks = Stock.objects.all()
     return render(request, "stocks.html", {"stocks": stocks})
+
+
+
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("products")
+    else:
+        form = ProductForm()
+    return render(request, "product_form.html", {"form": form, "title": "Добавить продукт"})
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("products")
+    else:
+        form = ProductForm(instance=product)
+    return render(request, "product_form.html", {"form": form, "title": "Редактировать продукт"})
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        product.delete()
+        return redirect("products")
+    return render(request, "product_confirm_delete.html", {"product": product})
